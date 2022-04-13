29142d818d60351d4aeb614ca961f0cbeb471966
李慎广 <lishenguang1@126.com>
2022-04-13 16:57:33 +0800

    feat: 增加nginx配置

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
- # 从第一行到最后一行分别表示：
- # 1. 生成数据库迁移文件
- # 2. 根据数据库迁移文件来修改数据库
- # 3. 用 uwsgi启动 django 服务, 不再使用python manage.py runserver
- python manage.py makemigrations&&
- python manage.py migrate&&
- uwsgi --ini ./uwsgi.ini
- # python manage.py runserver 0.0.0.0:8000
+# 从第一行到最后一行分别表示：
+# 1. 生成数据库迁移文件
+# 2. 根据数据库迁移文件来修改数据库
+# 3. 用 uwsgi启动 django 服务, 不再使用python manage.py runserver
+python3 manage.py collectstatic --noinput&&
+python3 manage.py makemigrations&&
+python3 manage.py migrate&&
+uwsgi --ini ./uwsgi.ini
+# python manage.py runserver 0.0.0.0:8000
