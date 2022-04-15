385241594accb0f95222727f07a9d0b604e4a774
李慎广 <lishenguang1@126.com>
2022-04-15 16:31:25 +0800

    fix: yml

--- start.sh
+++ start.sh
@@ -4,14 +4,14 @@
 # 2. 根据数据库迁移文件来修改数据库
 # 3. 用 uwsgi启动 django 服务, 不再使用python manage.py runserver
 # while ! nc -z db 3306 ; do   # TODO db找不到
-while ! nc -z 10.0.16.1 3306 ; do
+while ! nc -z 42.192.53.215 3306 ; do
     echo "Waiting for the MySQL Server"
     sleep 3
 done
 python3 manage.py collectstatic --noinput&&
 python3 manage.py makemigrations&&
 python3 manage.py migrate&&
-uwsgi --ini /var/www/html/testrd/uwsgi.ini&&
+uwsgi --ini ./uwsgi.ini&&
 tail -f /dev/null
 
 exec "$@"
