78873666aafcbec5e5e033877eb7275860b637bf
李慎广 <lishenguang1@126.com>
2022-04-21 15:11:27 +0800

    fix: 修改配置

--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -1,2 +1,3 @@
-GRANT ALL PRIVILEGES ON *.* TO root@"%" IDENTIFIED BY "password";
+create user root@'%' identified by '123456';
+grant all privileges on *.* to root@'%' with grant option;
 FLUSH PRIVILEGES;
