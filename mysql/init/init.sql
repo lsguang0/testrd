8afb607d66c833b04565b7de3ec77ecd5a5b9492
李慎广 <lishenguang1@126.com>
2022-04-21 18:47:01 +0800

    fix: 修改结构

--- mysql/init/init.sql
+++
@@ -1,3 +0,0 @@
-create user root@'%' identified by '123456';
-grant all privileges on *.* to root@'%' with grant option;
-FLUSH PRIVILEGES;
--- start.sh
+++ start.sh
@@ -1,7 +1,17 @@
-
+#!/bin/bash
+# 从第一行到最后一行分别表示：
+# 1. 生成数据库迁移文件
+# 2. 根据数据库迁移文件来修改数据库
+# 3. 用 uwsgi启动 django 服务, 不再使用python manage.py runserver
+# while ! nc -z db 3306 ; do   # TODO db找不到
+while ! nc -z 42.192.53.215 3306 ; do
+    echo "Waiting for the MySQL Server"
+    sleep 3
+done
 python3 manage.py makemigrations&&
 python3 manage.py migrate&&
 uwsgi --ini ./uwsgi.ini&&
 tail -f /dev/null
 
 exec "$@"
+# python manage.py runserver 0.0.0.0:8000
